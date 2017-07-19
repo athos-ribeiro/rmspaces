0a3cdaf3330d2d9560a74045be9e8ab60ab7f5d0
Athos Ribeiro <athoscr@fedoraproject.org>
2017-07-19 01:32:47 -0300

    Fix license fields for perl distribution

--- lib/App/rmspaces.pm
+++ lib/App/rmspaces.pm
@@ -1,6 +1,9 @@
 package App::rmspaces;
 
-our $VERSION = 0.3;
+use strict;
+use warnings;
+
+our $VERSION = 0.4;
 
 =head1 NAME
 
@@ -14,9 +17,22 @@ This is a stub module, see F<script/rmspaces> for details of the app.
 
 Athos Ribeiro
 
-=head1 LICENSE
+=head1 LICENSE AND COPYRIGHT
 
-GPLv3
+Copyright 2017 Athos Ribeiro <athoscr@fedoraproject.org>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 =cut
 
