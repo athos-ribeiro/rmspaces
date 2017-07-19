a7f7bd3dead54162fba6508d885d7ef3051d6022
Athos Ribeiro <athoscr@fedoraproject.org>
2017-07-18 21:07:02 -0300

    Make rmspaces a perl module under App::rmspaces

---
+++ lib/App/rmspaces.pm
@@ -0,0 +1,24 @@
+package App::rmspaces;
+
+our $VERSION = 0.02;
+
+=head1 NAME
+
+App::rmspaces - an app to remove spaces from file names
+
+=head1 DESCRIPTION
+
+This is a stub module, see F<script/rmspaces> for details of the app.
+
+=head1 AUTHOR
+
+Athos Ribeiro
+
+=head1 LICENSE
+
+GPLv3
+
+=cut
+
+1;
+
